b62f257993d064b7a2f66743ce342a26050480ae
Vitor Serigati de Oliveira Basso <vitor_serigati@hotmail.com>
2022-08-20 18:57:14 -0300

    Aula4 e Exercicio de fixacao finalizados

--- Aula4-Construtores-e-sobrecarga/Exercicio-Fixacao/Exerciciof-Conta/Program.cs
+++ Aula4-Construtores-e-sobrecarga/Exercicio-Fixacao/Exerciciof-Conta/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+namespace Exerciciof_Conta;
+
+class Program
+{
+    static void Main()
+    {
+        Conta conta;
+        double valor;
+        Console.Write("Entre o número da conta: ");
+        int numero = int.Parse(Console.ReadLine());
+        Console.Write("Entre o titular da conta: ");
+        string titular = Console.ReadLine();
+        Console.Write("Haverá depósito inicial? (s/n)?");
+        var resp = Console.ReadLine();
+
+        if (resp == "s" || resp == "S")
+        {
+            Console.Write("Entre o valor de depósito inicial: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta = new Conta(numero, titular, valor);
+        }
+        else
+        {
+            conta = new Conta(numero, titular);
+        }
+        Console.WriteLine("Dados da conta:");
+        Console.WriteLine(conta);
+
+        Console.Write("\nEntre um valor para o depósito: ");
+        valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        conta.Deposito(valor);
+        Console.WriteLine("Dados da conta:");
+        Console.WriteLine(conta);
+
+        Console.Write("\nEntre um valor para saque: ");
+        valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        conta.Saque(valor);
+        Console.WriteLine("Dados da conta:");
+        Console.WriteLine(conta);
+    }
+}
--- Aula4-Construtores-e-sobrecarga/Exercicio-Fixacao/Exerciciof-Conta/classes/Conta.cs
+++ Aula4-Construtores-e-sobrecarga/Exercicio-Fixacao/Exerciciof-Conta/classes/Conta.cs
@@ -1,22 +1,55 @@
 
+using System.Globalization;
+
 namespace Exerciciof_Conta;
 
 public class Conta
 {
+    private double Taxa = 5.0;
     public int Numero { get; private set; }
-    public string Nome { get; set; }
+    private string _nome { get; set; }
 
-    private double _saldo;
+    public double Saldo { get; private set; }
 
     public Conta(int numero, string nome)
     {
+        Saldo = 0.0;
         Numero = numero;
-        Nome = nome;
-        _saldo = 0.0;
+        if (nome != null && nome.Length > 1)
+        {
+            _nome = nome;
+
+        }
     }
 
     public Conta(int numero, string nome, double saldo) : this(numero, nome)
     {
-        _saldo = saldo;
+        Saldo = saldo;
+    }
+    public string Nome
+    {
+        get { return _nome; }
+        set
+        {
+            if (value != null && value.Length > 1)
+            {
+                _nome = value;
+            }
+        }
+    }
+    public void Deposito(double valor)
+    {
+        if (valor > 0)
+            Saldo += valor;
+    }
+    public void Saque(double valor)
+    {
+        if (valor > 0)
+            Saldo -= valor + Taxa;
+    }
+
+    public override string ToString()
+    {
+        return $"Conta: {Numero}, Titular: {_nome}, Saldo: ${Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
     }
 }

d248f035d8d3e03b659cc499ebcb307fc9d79782
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2023-01-26 20:02:55 -0300

    reorganizando as pastas

---
+++ Aula2-metodos-reaproveitamento/Exercicio/Exercicio-Produto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+namespace Exercicio_Produto;
+
+class Program
+{
+    static void Main()
+    {
+        Produto prod = new Produto();
+        Console.WriteLine("Entre com os dados do produto: ");
+        Console.WriteLine("Nome: ");
+        prod.Name = Console.ReadLine();
+        Console.WriteLine("Preço: ");
+        prod.Valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Quandidade em estoque: ");
+        prod.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine($"Dados do Produto: {prod}");
+        Console.WriteLine("\n\nDigite o número de produtos a ser adicionado ao estoque: ");
+        prod.AdicionarItens(int.Parse(Console.ReadLine()));
+        Console.WriteLine($"\nDados Atualizados: {prod}");
+        Console.WriteLine("\nDigite o número de produtos a ser retirado do estoque: ");
+        prod.RemoverItens(int.Parse(Console.ReadLine()));
+        Console.WriteLine($"\n\nDados Atualizados: {prod}");
+
+    }
+
+}
---
+++ Aula2-metodos-reaproveitamento/Exercicio_fixacao/exercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Exercicio_fixacao;
+
+class Program
+{
+
+    static void Main()
+    {
+        Retangulo r = new Retangulo();
+
+        Console.WriteLine("Entre a largura e altura do retângulo: ");
+        r.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        r.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine(r);
+    }
+}

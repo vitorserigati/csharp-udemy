4022f07f19fc36bd989a1d381f69593e7ab17150
Vitor Serigati <vitor_serigati@hotmail.com>
2023-06-10 12:16:42 -0300

    first lecture of generics

---
+++ 11-Generics-set-dictionary/Exercicios/Exercicio-1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Course;
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        PrintService service = new PrintService();
+
+        Console.WriteLine("How many values?");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Write the {i + 1}º number");
+            int x = int.Parse(Console.ReadLine());
+            service.AddValue(x);
+        }
+
+        service.Print();
+        Console.WriteLine($"First: {service.First()}");
+    }
+
+}
